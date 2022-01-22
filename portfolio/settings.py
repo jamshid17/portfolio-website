46b4c64a2291c6ff18e5f1088c6316ec918be08f
Jamshid Jabbarov <jamshidjabbarov17@gmail.com>
2022-01-22 15:00:43 +0500

    Automatic configuration with django-heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,4 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'main', 'templates'),
 )
-
+import django_heroku
+django_heroku.settings(locals())
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 with open("Procfile", 'rb') as f:
-    print(f.read().decode('utf-8'))
-    # with open("Procfile2", "w+b") as f2:
-    #     content = f.read()
-    #     f2.write(content.decode("utf-16").encode("utf-8"))
+    # print(f.read().decode('utf-8'))
+    with open("Procfile2", "w+b") as f2:
+        content = f.read()
+        f2.write(content.decode("utf-16").encode("utf-8"))
