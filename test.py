2a199093aff7f2a27c3eee42f40163a87299e9cb
Jamshid Jabbarov <jamshidjabbarov17@gmail.com>
2022-01-22 14:48:19 +0500

    Specify the command to run your project

---
+++ test.py
@@ -0,0 +1,5 @@
+with open("Procfile", 'rb') as f:
+    print(f.read().decode('utf-8'))
+    # with open("Procfile2", "w+b") as f2:
+    #     content = f.read()
+    #     f2.write(content.decode("utf-16").encode("utf-8"))
