f58132bf0a1aa8e1dc992d2f16915dfa8510246d
Jamshid Jabbarov <jamshidjabbarov17@gmail.com>
2022-01-23 12:58:29 +0500

    fixing runtime

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-with open("Procfile", 'rb') as f:
-    # print(f.read().decode('utf-8'))
-    with open("Procfile2", "w+b") as f2:
-        content = f.read()
-        f2.write(content.decode("utf-16").encode("utf-8"))
+with open("runtime.txt", 'rb') as f:
+    print(f.read().strip())
+    # with open("runtime2.txt", "w+b") as f2:
+    #     content = f.read()
+    #     f2.write(content.decode("utf-16").encode("utf-8"))
