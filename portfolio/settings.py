01bd72946c7308d85cc15b0d6844b0a8add8f894
Jamshid Jabbarov <jamshidjabbarov17@gmail.com>
2022-01-30 16:58:23 +0500

    debuggin

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = ['itsmejamshid.herokuapp.com']
-CSRF_TRUSTED_ORIGINS = ['https://itsmejamshid.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS = ['https://itsmejamshid.herokuapp.com']
+
 
 # Application definition
 
