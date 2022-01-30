7ab9509bd3408adb28d03d80b099d7f2e798d414
Jamshid Jabbarov <jamshidjabbarov17@gmail.com>
2022-01-30 16:42:04 +0500

    fixing allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','itsmejamshid.herokuapp.com']
+ALLOWED_HOSTS = ['itsmejamshid.herokuapp.com']
 
 
 # Application definition
