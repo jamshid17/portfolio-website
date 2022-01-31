121814441769cfe96bf799680338e3a1f910adf6
Jamshid Jabbarov <jamshidjabbarov17@gmail.com>
2022-01-31 10:48:56 +0500

    last timer mode before changing it

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-sf5m^26azf&0$#8s6z7wd4g279+^)7@@g0*v_1-hd9v%q9(*_p'
+SECRET_KEY = 'django-insecure-sf5m^26azf&0$#8s6z7wd4g279+^)7@@g0*v_1-hd9v%q9(*_p'
 # SECRET_KEY = os.environ['SECRET_KEY']
 # print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
